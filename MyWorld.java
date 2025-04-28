4ad0f4ab703f1f29633d8aed92e44130425a5672
AbithanP <abithan.mayo@gmail.com>
2025-04-28 14:49:19 -0400

    part 1 almostDone

--- MyWorld.java
+++ MyWorld.java
@@ -1,7 +1,43 @@
 import greenfoot.*;
 
+/**
+ * The World our hero lives in.
+ * 
+ * @author Abithan Paskaranathan
+ * @version April 2025
+ */
+
 public class MyWorld extends World {
+    public int score = 0;
+    Label scoreLabel;
+    
     public MyWorld() {
         super(600, 400, 1);
+        
+        Elephant elephant = new Elephant();
+        addObject(elephant, 300, 200);
+        
+        scoreLabel = new Label(0, 80);
+        addObject(scoreLabel, 50, 50);
+        
+        createApple();
+    }
+    
+    public void increaseScore()
+    {
+        score++;
+        scoreLabel.setValue(score);
+    }
+    
+    /**
+     * Creat a new apple at random location at top of screen
+     */
+    
+    public void createApple()
+    {
+        Apple apple = new Apple();
+        int x = Greenfoot.getRandomNumber(600);
+        int y = 0;
+        addObject(apple, x, y);
     }
 }
